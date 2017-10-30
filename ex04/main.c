7171e840cf074cc2073c6602baa3b1bb78f51b99
Harri Mähönen <mahonen.h@gmail.com>
2017-10-30 13:19:03 +0200

    ex04 improved logic

--- ex04/main.c
+++ ex04/main.c
@@ -9,53 +9,40 @@ int main()
     int _m;
     int _y;
 
+    int monthLength[12] = {31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
+
     printf("Enter today's  date (d.m.y): ");
     scanf("%d.%d.%d" ,&d, &m, &y);
+
     if (m < 13 && m > 0 && d < 32 && d > 0 ) {
-        if (m == 4 || m == 6 || m == 9 || m == 11) {
-            if (d > 31) {
-                invalid();
-                return 0;
-            }
-        }
-        if (m == 1 || m == 3 || m == 5 || m == 11) {
-            if (d > 30) {
-                invalid();
-                return 0;
-            }
-        }
-        if (m == 2) {
-            if (d > 28) {
-                invalid();
-                return 0;
-            }
+        if (d > monthLength[m-1]) {
+            invalid();
         }
         if (m > 12) {
             invalid();
-            return 0;
         }
         _d = d;
         _m = m;
         _y = y;
 
-        if (d == 30) {
+        if (d == monthLength[m-1]) {
             _d = 1;
             _m++;
-        } else if (d < 30) {
+        } else {
             _d++;
         }
-        if (d == 30 && m == 12) {
+        if (d == monthLength[m-1] && m == 12) {
             _m = 1;
             _y++;
         }
 
         printf("\nTomorrow is %d.%d.%d \n", _d, _m, _y);
         return 0;
+
     } else { invalid(); }
 };
 
-void invalid(){
+int invalid(){
     printf("Invalid date.\n");
+    return 0;
 };
-
-
